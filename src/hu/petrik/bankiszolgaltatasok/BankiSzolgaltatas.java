bcf4061e2d6d6e1d09eba0cc9125bdc26bb67f33
Bodor István <istvanbodor19@gmail.com>
2022-10-13 09:07:59 +0200

    BankiSzolgaltatas osztaly

---
+++ src/hu/petrik/bankiszolgaltatasok/BankiSzolgaltatas.java
@@ -0,0 +1,14 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public abstract class BankiSzolgaltatas {
+
+    private Tulajdonos tulajdonos;
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+}
